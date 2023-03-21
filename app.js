8eccf24f8a3592b3ba4d73c0fdcde564bb5afc6b
papiricoh <slendeman0200@gmail.com>
2023-03-21 09:58:19 +0100

    Added cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const routes = require('./app/routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // Configuración de middlewares y otros ajustes de la aplicación
 
+app.use(cors());
 app.use('/api', routes);
 
 // Configuración del puerto y inicio del servidor
